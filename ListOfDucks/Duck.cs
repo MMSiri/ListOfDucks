adcd79ed72e5e4efe79a5356338e974917d973d3
Mika Mirzanasiri <mika-mm@hotmail.com>
2022-08-25 18:54:09 +0100

    Add project files.

---
+++ ListOfDucks/Duck.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListOfDucks
+{
+    class Duck
+    {
+        public int Size { get; set; }
+        public KindOfDuck Kind { get; set; }
+
+    }
+}
